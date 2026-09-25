9b404b46899456ad1db60392c37dd9ee1b0ac67d
baloniritik25-maker <baloniritik25@gmail.com>
2026-09-25 07:33:55 +0530

    We want to make some def in board class and then call it by game

--- main.rb
+++ main.rb
@@ -1,22 +1,27 @@
 class Player
-    attr_accessor :move
+attr_accessor :move
   def initialize()
     print "Enter your move(1-9): "
-    self.move  = gets.chomp()
+    @move  = gets.chomp()
+     @move = @move.to_i
   end
 end
 
 
 class Board
-    
-    def initialize(player = Player.new)
+    attr_accessor :board
+    def initialize()
         
-        @board = Array.new(3,Array.new(3))
+         @board = Array.new(3) do
+            Array.new(3)
+         end
+        
+         print_board()
+    
+    end
 
-        puts player.move()
-        @player = player.move();
-        puts @player
-
+    def print_board
+              
         for i in 0...3
                
             for j in 0...3
@@ -33,18 +38,45 @@ class Board
             else
                 puts ""
             end
+        end     
+    end    
+    num = 89 ##here num will be the value  of player move we going to senf through game class at the object of board..t
+    def inappropriate_move(i,j)
+        if (num<1 || num >= 9)
+          return true
+        end
+        
+        if(@board[i][j].is_a?(String))
+          return true
         end
     end
+
+    
+    
          
 end
 
 
 class Game
-  
-end
- p1 = Player.new()
-  
- b1 = Board.new(p1);
+  def initialize
+    @board =  Board.new()
+  end
 
+  def start
+     p1 = Player.new();
+     
+     i = (p1.move / 3).floor
+     j = p1.move % 3 - 1
+     puts i,j
+
+     @board[i][j] = 'X'
+     @board.print_board();
+
+  end
+end
+ 
+  
+ g1 = Game.new()
+ g1.start()
  
  
